b2ca62b5fcf119ae1b95e85ca580cabe031a36ba
gaborkallos <gaborkallos@gmail.com>
2019-09-27 12:58:13 +0200

    basic with: objects, database connection

--- api/config/database.php
+++ api/config/database.php
@@ -1,14 +1,21 @@
 <?php
-class Database{
 
-    private $table_name = "autosalon";
+class Database
+{
+
+    private $db_name = "autosalon";
     private $conn;
 
-    public function getConnection(){
-        try{
-            $this->conn = new PDO("psql:host=localhost;dbname=" . $this->table_name, 'postgres', '547725');
+    public function getConnection()
+    {
+        try {
+            $this->conn = new PDO("psql:host=localhost;dbname=" . $this->db_name, 'postgres', '547725',
+                array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET utf8"
+            ));
             echo "Connection SUCCESS!";
-        }catch(PDOException $exception){
+        } catch (PDOException $exception) {
             echo "Connection error: " . $exception->getMessage();
         }
 
