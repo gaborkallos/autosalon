4ad2d10bfd6a3891a3621386aa52a0684b49a737
gaborkallos <gaborkallos@gmail.com>
2019-09-28 11:28:31 +0200

    getcars get method reply all cars from db

--- api/config/database.php
+++ api/config/database.php
@@ -10,24 +10,10 @@ class Database
     {
         $this->conn = new PDO("pgsql:host=localhost;dbname=" . $this->db_name, 'postgres', '547725'
         );
-        echo "Connection SUCCESS!";
     }
 
-    public function getConn(): ?PDO
+    public function getConnection(): ?PDO
     {
-        return $this->conn;
-    }
-
-    public function getConnection()
-    {
-        try {
-            $this->conn = new PDO("psql:host=localhost;dbname=" . $this->db_name, 'postgres', '547725'
-            );
-            echo "Connection SUCCESS!";
-        } catch (PDOException $exception) {
-            echo "Connection error: " . $exception->getMessage();
-        }
-
         return $this->conn;
     }
 
--- api/queries/getcars.php
+++ api/queries/getcars.php
@@ -1,43 +1,47 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
 
-include_once '../config/database.php';
-include_once '../objects/Car.php';
+    include_once '../config/database.php';
+    include_once '../objects/Car.php';
 
-$database = new Database();
-$conn = $database->getConn();
-$car = new Car($conn);
+    $database = new Database();
+    $conn = $database->getConnection();
+    $car = new Car($conn);
 
-$stmt = $car->get_all();
-$number_of_cars = $stmt->rowCount();
+    $stmt = $car->get_all();
+    $number_of_cars = $stmt->rowCount();
 
-if($number_of_cars>0){
-    $cars=array();
-    $cars["records"]=array();
+    if ($number_of_cars > 0) {
+        $cars = array();
+        $cars["records"] = array();
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        extract($row);
-        $newCar=array(
-            "id" => $id,
-            "manufacture" => $manufacture,
-            "model" => $model,
-            "year_of_manufacture" => $year_of_manufacture,
-            "price" => $price,
-            "administrator" => $new_employee=array(
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            $newCar = array(
                 "id" => $id,
-                "name" => $name,
-                "username" => $username
-            )
+                "manufacture" => $manufacture,
+                "model" => $model,
+                "year_of_manufacture" => $year_of_manufacture,
+                "price" => $price,
+                "administrator" => $new_employee = array(
+                    "name" => $name,
+                )
+            );
+            array_push($cars["records"], $newCar);
+        }
+        http_response_code(200);
+        echo json_encode($cars);
+    } else {
+        http_response_code(404);
+        echo json_encode(
+            array("message" => "No cars available!")
         );
-        array_push($cars["records"], $newCar);
     }
-    http_response_code(200);
-    echo json_encode($cars);
-}else{
-    http_response_code(404);
-    echo json_encode(
-        array("message" => "No cars available!")
-    );
+}
+    elseif ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    //TODO: code here the post request
+    echo "I got a POST!";
 }
